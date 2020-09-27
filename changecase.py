c600ad72dc92a6851d663e4114dc4a8f161a4ba8
2018HT13140 <2018HT13140@wilp.bits-pilani.ac.in>
2020-09-27 16:00:07 +0530

    Committed from Ubuntu, Exersices from Educative. Section 1 : Basic Data Types Program - Changecase

---
+++ changecase.py
@@ -0,0 +1,14 @@
+"""Given a function changeCase(s), the task is to convert the strings from upper case to lower case.
+
+Input : A string in upper case
+
+Output : Change case of the string in lower case
+"""
+def changecase(str):
+    convert2upper = str.upper(slower)
+    convert2lower = str.lower(supper)
+    return(convert2upper, convert2lower)
+
+slower = input("Enter a string in lower case: \n")
+supper = input("Enter a string in upper case: \n")
+print("String converted in uppercase is: \n" ,changecase(str))
